d8da81d02dcf560abe1ab36963587f204f9c9dc9
Tr0y4n <55826971+Tr0y4n@users.noreply.github.com>
2022-05-25 21:08:29 +0300

    pizda ebanaya

--- front/components/RecComponent/RecComponent.js
+++ front/components/RecComponent/RecComponent.js
@@ -4,7 +4,7 @@ import './RecComponent.css';
 import DeviceItem from '../DeviceList/DeviceItem';
 
 export default function RecComponent(props) {
- const recs = props.recs
+ const recs = props.recs || []
  //console.log("recs in main = ", recs)
   return (
     <Row className="field">
